cec767acabeb54901c2a0ff352cc135e1586a26e
pranav-vasu <pranav@codenimbussolutions.com>
2024-02-06 12:02:22 +0530

    vite local host changes

--- invitaion-react/src/pages/Chat.jsx
+++ invitaion-react/src/pages/Chat.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import SideBar from "../components/SideBar";
 import TopBar from "../components/TopBar";
 import ChatAreaSearch from "../assets/ChatBarImg/ChatAreaSearch.svg";
@@ -6,10 +7,30 @@ import ChatDecoration from "../assets/ChatBarImg/ChatDecoration.svg";
 import Payment from "../assets/ChatBarImg/Payment.svg";
 import Search from "../assets/ChatBarImg/Search.svg";
 import UserIcon from "../assets/ChatBarImg/UserIcon.svg";
+import Send from "../assets/ChatBarImg/Send.svg";
 
 import "./Chat.css";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    const message = e.target.message.value;
+    setMessages([...messages, { message }]);
+    e.target.reset();
+  };
+
+  const displayMessages = () => {
+    return messages.map((message, index) => {
+      return (
+        <li className="msg" key={index}>
+          {message.message}
+        </li>
+      );
+    });
+  };
+
   return (
     <>
       <div className="chat-container">
@@ -20,7 +41,7 @@ const Chat = () => {
           <div className="top-bar">
             <TopBar></TopBar>
           </div>
-          <div className="main-area">
+          <div className="area-main">
             <div className="chat-bar">
               <h3 className="chat-bar-title">Recent Chat</h3>
               <div className="search-bar-box">
@@ -73,7 +94,50 @@ const Chat = () => {
                 </div>
               </div>
             </div>
-            <div className="chat-area"></div>
+            <div className="chat-area">
+              <div className="chat-title-bar">
+                <div className="chat-tags">
+                  <h3 className="chat-user-name">Username 01</h3>
+                  <span>Buyer</span>
+                  <span>WED1234</span>
+                </div>
+                <div className="chat-icons">
+                  <div className="chat-icon">
+                    <img src={ChatAreaSearch} alt="" />
+                  </div>
+                  <div className="chat-icon">
+                    <img src={ChatDecoration} alt="" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="chat-message-box">
+                <ul className="chat-msg">{displayMessages()}</ul>
+
+                <form
+                  className="send-msg"
+                  action=""
+                  onSubmit={handleSendMessage}
+                >
+                  <input
+                    type="text"
+                    name="message"
+                    id=""
+                    placeholder="Send your message"
+                  />
+                  <div className="send-msg-icons">
+                    <div className="msg-icon">
+                      <img src={Payment} alt="" />
+                    </div>
+                    <button type="submit">
+                      <div className="msg-icon">
+                        <img src={Send} alt="" />
+                      </div>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
