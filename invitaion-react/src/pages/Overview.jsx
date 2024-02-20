9c42874b88b6eb13987c04626cca014268273a09
pranav-vasu <pranav@codenimbussolutions.com>
2024-02-20 20:33:46 +0530

    fixed overview page layout

--- invitaion-react/src/pages/Overview.jsx
+++ invitaion-react/src/pages/Overview.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import SideBar from "../components/SideBar";
 import TopBar from "../components/TopBar";
+import "./Overview.css";
 
 const Overview = () => {
   return (
     <>
-      <TopBar></TopBar>
-      <SideBar></SideBar>
+      <div className="templates-container">
+        <div className="sidebar">
+          <SideBar></SideBar>
+        </div>
+        <div className="main-area-box">
+          <div className="top-bar">
+            <TopBar></TopBar>
+          </div>
+          <div className="main-area"></div>
+        </div>
+      </div>
     </>
   );
 };
