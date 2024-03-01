3732a0e1f94c1dd51c17c63fb6ba0843454ae2a1
Harish <harish@codenimbussolutions.com>
2024-03-02 01:22:42 +0530

    quick fix

--- invitaion-react/vite.config.js
+++ invitaion-react/vite.config.js
@@ -2,15 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "127.0.0.1",
+//     port: 8000,
+//   },
+//   build: {
+//     rollupOptions: {
+//       input: "src/main.jsx", // Adjust the path accordingly
+//     },
+//   }
+// });
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "127.0.0.1",
-    port: 8000,
-  },
-  build: {
-    rollupOptions: {
-      input: "src/main.jsx", // Adjust the path accordingly
-    },
-  },
-};
+        host: "127.0.0.1",
+        port: 8000,
+      },
+})
