21a99ce73a4f6994a623963719008255768d8730
pranav-vasu <pranav@codenimbussolutions.com>
2024-03-28 19:46:45 +0530

    commit index css

--- invitaion-react/src/components/pages/Dashboard/Homepage.jsx
+++ invitaion-react/src/components/pages/Dashboard/Homepage.jsx
@@ -1,38 +1,47 @@
-import BadgeAvatars from '../../MUI components/avatar';
-import '../../../components/dash.css'
-import  { Sidebar } from '../../MUI components/sidebar';
-
-
+import BadgeAvatars from "../../MUI components/avatar";
+import "../../../components/dash.css";
+import { Sidebar } from "../../MUI components/sidebar";
 
 import { Websetup } from "../../MUI components/websetupcrd";
-import Mobilehero from '../../Mobilecomponent/mobilehero';
-import LabelBottomNavigation from '../../MUI components/Bottomnavigation';
-import ImageUpload from '../../MUI components/imageupload';
-import Navbar from '../../MUI components/Navbar';
+import Mobilehero from "../../Mobilecomponent/mobilehero";
+import LabelBottomNavigation from "../../MUI components/Bottomnavigation";
+import ImageUpload from "../../MUI components/imageupload";
+import Navbar from "../../MUI components/Navbar";
 
-export function Homepage(){
-    return(
-        <div className="mainhomepg">
-            <div className="mobherodev">
-                <Mobilehero/>
-            </div>
-           <div className="navbar">
-            <Navbar/>
-           </div>
-          <div className="sideweb">
-           
-                <Sidebar/>
-          
-          
-          <div className="websetup" style={{zIndex:"1", }}>
-            <Websetup/>
-          </div></div>
-         
-            
-            <div className="bottomnav" >
-                <LabelBottomNavigation/>
-            </div>
-           
+export function Homepage() {
+  return (
+    // <div className="mainhomepg">
+    //     <div className="mobherodev">
+    //         <Mobilehero/>
+    //     </div>
+    //    <div className="navbar">
+    //     <Navbar/>
+    //    </div>
+    //   <div className="sideweb">
+
+    //         <Sidebar/>
+
+    //   <div className="websetup" style={{zIndex:"1", }}>
+    //     <Websetup/>
+    //   </div></div>
+
+    //     <div className="bottomnav" >
+    //         <LabelBottomNavigation/>
+    //     </div>
+
+    // </div>
+    <>
+      <div className="templates-container">
+        <div className="sidebar">
+          <Sidebar />
         </div>
-    )
+        <div className="main-area-box">
+          <div className="top-bar">
+            <Navbar />
+          </div>
+          <div className="main-area"></div>
+        </div>
+      </div>
+    </>
+  );
 }
